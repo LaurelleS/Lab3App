12d462e7fe4d7c94c5460c9815ba804eeebf8a58
LaurelleS <laurelle887@gmail.com>
2023-12-09 23:25:54 -0600

    Added type hinting and docstrings

--- formulas.py
+++ formulas.py
@@ -1,7 +1,12 @@
 import random
 
 
-def add(values):
+def add(values: [float]) -> float:
+    """
+    Adds all positive numbers in values
+    :param values: [float]
+    return: float
+    """
     tot = 0
     for num in values:
         if num > 0:
@@ -9,7 +14,12 @@ def add(values):
     return tot
 
 
-def subtract(values):
+def subtract(values: [float]) -> float:
+    """
+    Sums up all negative numbers in values
+    :param values: [float]
+    :return: float
+    """
     negSum = 0
     for num in values:
         if num < 0:
@@ -17,7 +27,12 @@ def subtract(values):
     return negSum
 
 
-def multiply(values):
+def multiply(values: [float]) -> float:
+    """
+    Multiplies all non-zero numbers in values
+    :param values: [float]
+    :return: float
+    """
     product = values[0]
     for num in values[1:]:
         if not num == 0:
@@ -25,7 +40,12 @@ def multiply(values):
     return product
 
 
-def divide(values):
+def divide(values: [float]) -> float:
+    """
+    Divides the numbers in values in order
+    :param values: [float]
+    :return: float
+    """
     quo = values[0]
     if values[0] == 0:
         quo = 0
@@ -37,5 +57,10 @@ def divide(values):
     return quo
 
 
-def choose(values):
+def choose(values: [float]) -> float:
+    """
+    Chooses a random number in values
+    :param values: [float]
+    :return: float
+    """
     return random.choice(values)
--- logic.py
+++ logic.py
@@ -15,6 +15,9 @@ class Logic(QMainWindow, Ui_MainWindow):
         self.button_clear.clicked.connect(lambda: self.clear())
 
     def enter(self):
+        """
+        Saves numbers to be calculated
+        """
         entry = self.line_entry.text()
         try:
             self.num_list.append(float(entry))
@@ -25,6 +28,9 @@ class Logic(QMainWindow, Ui_MainWindow):
         self.line_entry.clear()
 
     def calc(self):
+        """
+        calculates values with selected operand
+        """
         if len(self.num_list) == 0:
             self.label.setText('0')
         elif self.radio_add.isChecked():
@@ -45,6 +51,9 @@ class Logic(QMainWindow, Ui_MainWindow):
             self.label.setText(str(formulas.choose(self.num_list)))
 
     def del_entry(self):
+        """
+        Deletes selected number
+        """
         tar = self.list.currentRow()
         self.num_list.remove(self.num_list[tar - 1])
         self.list.takeItem(tar)
@@ -52,6 +61,9 @@ class Logic(QMainWindow, Ui_MainWindow):
             self.button_del.setEnabled(False)
 
     def clear(self):
+        """
+        Resets Gui
+        """
         self.line_entry.clear()
         self.list.setCurrentRow(1)
         for i in range(len(self.num_list)):
